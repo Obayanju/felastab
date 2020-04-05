441c42465c254b2dd6a271e09e21682e00337e2c
Dami <obayanjud@gmail.com>
2020-04-05 01:20:16 -0600

    add cors

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"log"
 
+	"github.com/gofiber/cors"
 	"github.com/gofiber/fiber"
 	"github.com/obayanju/felastab/search"
 )
 
 func main() {
-	type Code struct {
-		Text string
+	type CodeData struct {
+		Code string
 	}
 	app := fiber.New()
 
+	app.Use(cors.New())
+
 	app.Post("/", func(c *fiber.Ctx) {
-		var code Code
-		if err := c.BodyParser(&code); err != nil {
+		var codeData CodeData
+		if err := c.BodyParser(&codeData); err != nil {
 			log.Fatal(err)
 		}
-		//fmt.Printf("%v\n", code.Text)
-		search.Start(code.Text)
+		search.Start(codeData.Code)
 	})
 
 	app.Listen(3001)
